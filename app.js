7689866504f01f3fd5f1910b9aedd2fe0c19fd2e
Carlomadella <madella871@gmail.com>
2025-06-03 21:37:31 +0200

    [UPD] inserite istruzioni base nel file app.js

---
+++ app.js
@@ -0,0 +1,24 @@
+// importo express
+const express = require('express');
+
+// inizializzo la variabile app con il metodo express()
+const app = express();
+
+// definisco il numero di porta su cui il server deve rimanere in ascolto
+const port = 3000;
+
+// uso il middleware per gli asset statici
+app.use(express.static('public'));
+
+// uso il middleware per il parsing del body delle richieste
+app.use(express.json());
+
+// definisco un entry point
+app.get("/", (req, res) => {
+    res.send('Movies API server');
+});
+
+// dico al server di rimanere in ascolto sulla porta 3000
+app.listen(port, () => {
+    console.log(`Server is listening on port ${port}`);
+});
